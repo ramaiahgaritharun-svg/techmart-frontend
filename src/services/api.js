d8bd7bc785e739b0174239217545c1bd7fe5f3bf
Ramaiahgari Tharun <ramaiahgaritharun2200@gmail.com>
2026-07-11 18:36:36 +0530

    Change token refresh API URL to BASE_URL
    Updated API URL for token refresh endpoint.

--- src/services/api.js
+++ src/services/api.js
@@ -31,7 +31,7 @@ api.interceptors.response.use(
         const refresh = localStorage.getItem("refresh");
 
         const res = await axios.post(
-          `${import.meta.env.VITE_API_URL}/token/refresh/`,
+          `${BASE_URL}/token/refresh/`
           { refresh },
         );
 
